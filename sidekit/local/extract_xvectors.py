f344f9a8a3951a516664beb7d7aef457539864c2
pchampio <prr.champion@gmail.com>
2021-10-08 14:45:01 +0200

    fix install sidekit & first version of extract_xvector

--- sidekit/local/extract_xvectors.py
+++ sidekit/local/extract_xvectors.py
@@ -7,26 +7,28 @@ import argparse
 @torch.no_grad()
 def main(model_path, device):
     device = torch.device(device)
-    model = torch.load(model_path, map_location=device)
+    model_config = torch.load(model_path, map_location=device)
 
-    model_opts = model["model_archi"]
-    model = Xtractor(model["speaker_number"],
+    model_opts = model_config["model_archi"]
+    if "embedding_size" not in model_opts:
+        model_opts["embedding_size"] = 256
+    xtractor = Xtractor(model_config["speaker_number"],
                      model_archi=model_opts["model_type"],
                      loss=model_opts["loss"]["type"],
                      embedding_size=model_opts["embedding_size"])
 
-    model.load_state_dict(model["model_state_dict"], strict=True)
-    model.to(device)
-    model.eval()
+    xtractor.load_state_dict(model_config["model_state_dict"], strict=True)
+    xtractor = xtractor.to(device)
+    xtractor.eval()
     #  signal, sr = torchaudio.load("/home/hnourtel/Documents/Test/wav/Ses01M_Ses01M_impro04_M010_anon.wav")
     signal = torch.rand(1, 3000)
-    _, vec = model(signal, is_eval=True)
+    signal = signal.to(device)
+    _, vec = xtractor(signal, is_eval=True)
     print(vec.size())
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Extract the x-vectors given a sidekit model")
-    parser.add_argument("model", type=str)
+    parser.add_argument("--model", type=str, help="SideKit model", required=True)
     parser.add_argument("--device", default="cuda"if torch.cuda.is_available() else "cpu", type=str, help="The device (cpu or cuda:0) to run the inferance")
-
     args = parser.parse_args()
-    main(args["model"])
+    main(args.model, args.device)
