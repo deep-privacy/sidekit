486a4fcc4afc3c95b9508e559ec56e67145fa9c5
hnourtel <hubert.nourtel@loria.fr>
2021-11-03 12:20:27 +0100

    Adding comments to compare_results and fixing bugs

--- egs/vpc2020_decode/local/compare_results.py
+++ egs/vpc2020_decode/local/compare_results.py
@@ -3,15 +3,33 @@ from collections import OrderedDict
 from tabulate import tabulate
 
 def main():
+    """
+    Parameters to modify for customizing processing
+    """
+    # Dict of result files to process.
+    # Key is test name and value is a dict with two keys :
+    #   - "display" : Title of the test. It will be displayed on final table
+    #   - "file_path" : path to the result file to process. Path is absolute or relative to the working directory
+    # First file in this dict is the reference for value comparison between tests (variation percentage displayed is variation compared to first file result)
     results_path_dict = OrderedDict(
-        VPC2020_libri={"display": "VPC 2020 with Kaldi\nTrained on Libri-train-clean-360", "file_path": "results_vpc2020.txt"},
-        sidekit_vox_aug={"display": "VPC 2020 with Sidekit\nTrained on Voxceleb12 + Data augmentation", "file_path": "results.txt"},
-        sidekit_scratch_libri_aug={"display": "VPC 2020 with Sidekit\nTrained on Libri-train-clean-360 + Data augmentation\nTraining duration : 10h07", "file_path": "results_scratch_aug.txt"},
-        sidekit_transfer_vox_libri_aug={"display": "VPC 2020 with Sidekit\nTrained on Voxceleb12, transfer learning on Libri-train-clean-360 + Data augmentation\nTraining duration : 14h54", "file_path": "results_transfer_aug.txt"}
+        VPC2020_libri={"display": "VPC 2020 with Kaldi\nTrain : Libri-train-clean-360", "file_path": "results_vpc2020.txt"},
+        sidekit_vox_aug={"display": "VPC 2020 with Sidekit\nTrain : Voxceleb12 + Data augmentation", "file_path": "results.txt"},
+        sidekit_scratch_libri_aug={"display": "VPC 2020 with Sidekit\nTrain : Libri-train-clean-360 - Augmentation\nTraining duration : 10h07", "file_path": "results_scratch_aug.txt"},
+        sidekit_transfer_vox_libri_aug={"display": "VPC 2020 with Sidekit\nTrain : Voxceleb12, transfer Libri-train-clean-360 - Augmentation\nTraining duration : 14h54", "file_path": "results_transfer_aug.txt"},
+        sidekit_scratch_libri_noaug={"display": "VPC 2020 with Sidekit\nTrain : Libri-train-clean-360 - No augmentation\nTraining duration : 7h41", "file_path": "results_scratch_noaug.txt"},
+        sidekit_transfer_vox_libri_noaug={"display": "VPC 2020 with Sidekit\nTrain : Voxceleb12, transfer Libri-train-clean-360 - No augmentation\nTraining duration : 6h47", "file_path": "results_transfer_noaug.txt"}
+
     )
+    # Metrics to display on the final table. Leave empty to display all metrics
+    metrics_to_display = ["EER"]
+    # Out file path for result table
+    out_filepath = "compare_res.txt"
+
+
 
     results = OrderedDict() # Key = task, value = [[test name, EER, Cllr, Linkability]]
     display_tab_column_name = ["Dataset"]
+    nb_res_file_processed = 0
     for test_name, test_data in results_path_dict.items():
         text_test_display = test_data["display"]
         file_path = test_data["file_path"]
@@ -31,6 +49,11 @@ def main():
                 head_line = head_line[4:].strip()  # Remove ASV- or ASV:
                 split_head = head_line.split("-")
                 dataset = split_head[0].strip() + " - " + split_head[1].strip()
+
+                # Dataset tests are based on first result file.
+                # If it's not the first result file, check if dataset is already present. If it's not the case raise error.
+                if nb_res_file_processed > 0 and dataset not in results:
+                    raise ValueError("Current dataset not found in first result file : ", dataset)
 
                 results.setdefault(dataset, {"EER": [], "Cllr": [], "Linkability": []})
 
@@ -62,23 +85,44 @@ def main():
                 else:
                     linkability_val = "N/A"
                 results[dataset]["Linkability"].append(linkability_val)
+        nb_res_file_processed += 1
+        results = control_result_list(results, nb_res_file_processed)
         display_tab_column_name.append(text_test_display)
 
     tab_rows = []
-    display_list = ["EER"]
     for dataset, data in results.items():
         for res_name, res_data in data.items():
-            if len(display_list) == 0 or (res_name in display_list):
+            if len(metrics_to_display) == 0 or (res_name in metrics_to_display):
                 new_row = [dataset + " - " + res_name]
                 new_row.extend(data[res_name])
                 tab_rows.append(new_row)
 
     compare_res_table = tabulate(tab_rows, headers=display_tab_column_name, tablefmt="orgtbl")
     print(compare_res_table)
-    out_file = open("compare_res.txt", "w")
+    out_file = open(out_filepath, "w")
     out_file.write(compare_res_table)
     out_file.close()
 
+def control_result_list(result_list, nb_test_done):
+    """
+    Control if a result is missing in result_list. Useful if a dataset is not found in current file but found in result_list
+    If a result is missing, adding "N/A" to the result list.
+
+    Parameters
+    ----------
+    result_list: Dict of results already processed
+    nb_test_done : Current number of tests done at this point
+
+    Returns
+    -------
+    The result_list with "N/A" values if a test is missing
+    """
+    for dataset, data in result_list.items():
+        for metricType, metricList in data.items():
+            if len(metricList) < nb_test_done:
+                result_list[dataset][metricType].append("N/A")
+
+    return result_list
 
 
 
