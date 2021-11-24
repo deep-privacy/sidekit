c4941e97f1738c6a8b5243e90986acf1d334c9a5
hnourtel <hubert.nourtel@loria.fr>
2021-11-24 16:58:48 +0100

    Refactoring compare_results

--- egs/vpc2020_decode/local/compare_results.py
+++ egs/vpc2020_decode/local/compare_results.py
@@ -2,25 +2,35 @@
 from collections import OrderedDict
 from tabulate import tabulate
 import csv
+from argparse import ArgumentParser
 
 def main():
     """
     Parameters to modify for customizing processing
     """
+
+    parser = ArgumentParser()
+    parser.add_argument('--output_filepath', type=str, default="compare_res", help="Path for output files. Don't include extension as multiple files will be created")
+    args = parser.parse_args()
+
     # Dict of result files to process.
     # Key is test name and value is a dict with two keys :
     #   - "display" : Title of the test. It will be displayed on final table
     #   - "file_path" : path to the result file to process. Path is absolute or relative to the working directory
     # First file in this dict is the reference for value comparison between tests (variation percentage displayed is variation compared to first file result)
     results_path_dict = OrderedDict(
+        # VPC2020_libri={"display": "VPC 2020 with Kaldi\nTrain : Libri-train-clean-360_anon", "file_path": "results_vpc2020_anon.txt"},
+        # sidekit_anon_scratch_aug={"display": "VPC 2020 with Sidekit\nTrain : Libri-train-clean-360_anon_spk - Augmentation", "file_path": "results_anon_spk_scratch_aug.txt"},
+        # sidekit_anon_scratch_noaug={"display": "VPC 2020 with Sidekit\nTrain : Libri-train-clean-360_anon_spk - No augmentation", "file_path": "results_anon_spk_scratch_no_aug.txt"},
+
         VPC2020_libri={"display": "VPC 2020 with Kaldi\nTrain : Libri-train-clean-360", "file_path": "results_vpc2020.txt"},
         #sidekit_vox_aug={"display": "VPC 2020 with Sidekit\nTrain : Voxceleb12 - Augmentation", "file_path": "results.txt"},
-        sidekit_scratch_libri_aug={"display": "VPC 2020 with Sidekit\nTrain : Libri-train-clean-360 - Augmentation\nTraining duration : 10h07", "file_path": "results_scratch_aug.txt"},
+        sidekit_scratch_libri_aug={"display": "VPC 2020 with Sidekit\nTrain : Libri-train-clean-360 - Augmentation", "file_path": "results_scratch_aug.txt"},
         #sidekit_transfer_vox_libri_aug={"display": "VPC 2020 with Sidekit\nTrain : Voxceleb12, transfer Libri-train-clean-360 - Augmentation\nTraining duration : 14h54", "file_path": "results_transfer_aug.txt"},
-        sidekit_scratch_libri_noaug={"display": "VPC 2020 with Sidekit\nTrain : Libri-train-clean-360 - No augmentation\nTraining duration : 7h41", "file_path": "results_scratch_noaug.txt"},
+        sidekit_scratch_libri_noaug={"display": "VPC 2020 with Sidekit\nTrain : Libri-train-clean-360 - No augmentation", "file_path": "results_scratch_noaug.txt"},
         #sidekit_transfer_vox_libri_noaug={"display": "VPC 2020 with Sidekit\nTrain : Voxceleb12, transfer Libri-train-clean-360 - No augmentation\nTraining duration : 6h47", "file_path": "results_transfer_noaug.txt"},
-        sidekit_anon_scratch_aug={"display": "VPC 2020 with Sidekit\nTrain : Libri-train-clean-360_anon - Augmentation", "file_path": "results_anon_spk_scratch_aug.txt"},
-        sidekit_anon_scratch_noaug={"display": "VPC 2020 with Sidekit\nTrain : Libri-train-clean-360_anon - No augmentation", "file_path": "results_anon_spk_scratch_no_aug.txt"},
+        #sidekit_anon_scratch_aug={"display": "VPC 2020 with Sidekit\nTrain : Libri-train-clean-360_anon - Augmentation", "file_path": "results_anon_spk_scratch_aug.txt"},
+        #sidekit_anon_scratch_noaug={"display": "VPC 2020 with Sidekit\nTrain : Libri-train-clean-360_anon - No augmentation", "file_path": "results_anon_spk_scratch_no_aug.txt"},
         #sidekit_anon_transfer_aug={"display": "VPC 2020 with Sidekit\nTrain : Voxceleb12, transfer Libri-train-clean-360_anon - Augmentation", "file_path": "results_anon_spk_transfer_aug.txt"},
         #sidekit_anon_transfer_noaug={"display": "VPC 2020 with Sidekit\nTrain : Voxceleb12, transfer Libri-train-clean-360_anon - No augmentation", "file_path": "results_anon_spk_transfer_no_aug.txt"},
         sidekit_vad_scratch_aug={"display": "VPC 2020 with Sidekit\nTrain : Libri-train-clean-360 with VAD - Augmentation", "file_path": "results_vad_scratch_aug.txt"},
@@ -29,13 +39,18 @@ def main():
         #sidekit_vad_transfer_noaug={"display": "VPC 2020 with Sidekit\nTrain : Voxceleb12, transfer Libri-train-clean-360 with VAD - No augmentation", "file_path": "results_vad_transfer_no_aug.txt"},
 
     )
-    # Metrics to display on the final table. Leave empty to display all metrics
+
+    # Metrics to display on the final table.
+    # Current accepted metrics (ATTENTION : case sensitive) : "EER", "linkability", "Cllr"
     metrics_to_display = ["EER"]
-    # Out file path for result table
-    out_filepath = "compare_res" # Path without file extension. csv or txt extension will be added automatically
+
+    # Out file path for compare results
+    # Path without file extension. csv or txt extension will be added automatically
+    out_filepath = args.output_filepath
 
 
 
+    # Start processing result files
     results = OrderedDict() # Key = task, value = [[test name, EER, Cllr, Linkability]]
     display_tab_column_name = ["Dataset"]
     nb_res_file_processed = 0
@@ -45,64 +60,86 @@ def main():
         print("New result file : ", test_name, " - ", file_path)
 
         with open(file_path, "r") as result_file:
+            skip_task = False
             while True:
-                # Read header of result task. It can raise StopIteration if no more task exists
                 try:
-                    head_line = result_file.readline()
-                    if head_line.strip() == "" or "ASR" in head_line:
+                    cur_line = result_file.readline().strip()
+                    if cur_line.strip() == "" or cur_line.startswith("ASR"):
                         # Stop iteration if a blank line is found or if ASR result part is reached
                         raise StopIteration
+                    if skip_task:
+                        # Skipping current task requested. Checking if a new task is reached.
+                        # If yes, stop skipping and processing the task
+                        # If not, go directly to next iteration to read next line
+                        if cur_line.startswith("ASV"):
+                            skip_task = False
+                        else:
+                            continue
                 except StopIteration:
                     break # Stop loop as no more task are loaded
-                # Extract dataset name from header
-                head_line = head_line[4:].strip()  # Remove ASV- or ASV:
-                split_head = head_line.split("-")
-                dataset = split_head[0].strip() + " - " + split_head[1].strip()
 
-                # Dataset tests are based on first result file.
-                # If it's not the first result file, check if dataset is already present. If it's not the case raise error.
-                if nb_res_file_processed > 0 and dataset not in results:
-                    raise ValueError("Current dataset not found in first result file : ", dataset)
+                # Line containing ASV => header line of the task
+                if cur_line.startswith("ASV"):
+                    # Extract cur_dataset name
+                    head_line = cur_line[4:].strip()  # Remove ASV- or ASV:
+                    split_head = head_line.split("-")
+                    cur_dataset = split_head[0].strip() + " - " + split_head[1].strip()
 
-                results.setdefault(dataset, {"EER": [], "Cllr": [], "Linkability": []})
+                    # Checking if new dataset exists in first result file (reference).
+                    # If current file is the first, ignore this checking as first file is used as reference
+                    if nb_res_file_processed > 0 and cur_dataset not in results:
+                        print("Current dataset not found in first result file : ", cur_dataset, ". Skipping current dataset")
+                        # Read until next header
+                        skip_task = True
+                        # Go to next iteration for reading next line and skip the current task
+                        continue
 
-                # Read EER
-                eer_line = result_file.readline()
-                eer_split = eer_line.split(":")
-                eer_val = eer_split[1].replace("%", "").strip()
-                if len(results[dataset]["EER"]) > 0:
-                    first_val = results[dataset]["EER"][0].replace("%", "").strip()
-                    diff_percent = ((float(eer_val) - float(first_val)) * 100) / float(first_val)
-                    diff_percent_display = " ({:+.2f} %)".format(diff_percent)
-                else:
-                    diff_percent_display = ""
-                eer_val_display = eer_val + " %" + diff_percent_display
-                results[dataset]["EER"].append(eer_val_display)
+                    # Adding new task in result list if not already present in result list
+                    results.setdefault(cur_dataset, {metric: [] for metric in metrics_to_display})
 
-                # Read Cllr
-                cllr_line = result_file.readline()
-                cllr_split = cllr_line.split(":")
-                cllr_val = cllr_split[1].replace(" ", "/").strip()
-                cllr_val_display = cllr_val
-                results[dataset]["Cllr"].append(cllr_val_display)
+                # Retrieve EER result
+                # Current line contains EER result and EER is a metric to keep
+                if cur_line.startswith("EER") and "EER" in results[cur_dataset]:
+                    eer_split = cur_line.split(":")
+                    eer_val = eer_split[1].strip().replace("%", "")
+                    # Calculate percentage difference with first result file. Only occurs if at least one file was processed
+                    if nb_res_file_processed > 0:
+                        first_val = results[cur_dataset]["EER"][0].replace("%", "").strip()
+                        # Don't calculate difference if first result is not floating number => cast first_val to float will raise ValueError exception
+                        try:
+                            diff_percent = ((float(eer_val) - float(first_val)) * 100) / float(first_val)
+                            diff_percent_display = "({:+.2f} %)".format(diff_percent)
+                        except ValueError:
+                            diff_percent_display = "(NaN%)"
+                    else:
+                        diff_percent_display = ""
+                    eer_val_display = eer_val + " % " + diff_percent_display
+                    results[cur_dataset]["EER"].append(eer_val_display)
 
-                # Read linkability
-                linkability_line = result_file.readline()
-                if "linkability" in linkability_line:
-                    linkability_split = linkability_line.split(":")
+                # Retrieve Cllr result
+                # Current line contains Cllr result and Cllr is a metric to keep
+                if cur_line.startswith("Cllr") and "Cllr" in results[cur_dataset]:
+                    cllr_split = cur_line.split(":")
+                    cllr_val = cllr_split[1].strip().replace(" ", "/")
+                    cllr_val_display = cllr_val
+                    results[cur_dataset]["Cllr"].append(cllr_val_display)
+
+                # Retrieve Linkability result
+                # Current line contains Linkability result and Linkability is a metric to keep
+                if cur_line.startswith("linkability") and "linkability" in results[cur_dataset]:
+                    linkability_split = cur_line.split(":")
                     linkability_val = linkability_split[1].strip()
-                else:
-                    linkability_val = "N/A"
-                results[dataset]["Linkability"].append(linkability_val)
+                    results[cur_dataset]["linkability"].append(linkability_val)
+
         nb_res_file_processed += 1
         results = control_result_list(results, nb_res_file_processed)
         display_tab_column_name.append(text_test_display)
 
     tab_rows = []
-    for dataset, data in results.items():
+    for cur_dataset, data in results.items():
         for res_name, res_data in data.items():
-            if len(metrics_to_display) == 0 or (res_name in metrics_to_display):
-                new_row = [dataset + " - " + res_name]
+            if res_name in metrics_to_display:
+                new_row = [cur_dataset + " - " + res_name]
                 new_row.extend(data[res_name])
                 tab_rows.append(new_row)
 
