35ba0d9afc5b9a69261233412585a81c8b5ad2df
hnourtel <hubert.nourtel@loria.fr>
2021-11-25 10:16:20 +0100

    Adding training script and parameters for Voxceleb1&2

---
+++ egs/voxceleb12/train.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export NUM_NODES=1
+export NUM_GPUS_PER_NODE=2
+export NODE_RANK=0
+export WORLD_SIZE=$(($NUM_NODES * $NUM_GPUS_PER_NODE))
+
+mkdir -p model log
+cd ../..
+. ./env.sh
+cd -
+
+dataset_file="cfg/Voxceleb.yaml"
+model_file="cfg/model.yaml"
+training_file="cfg/training.yaml"
+
+python3 -m torch.distributed.launch \
+       --nproc_per_node=$NUM_GPUS_PER_NODE \
+       --nnodes=$NUM_NODES \
+       --node_rank $NODE_RANK \
+       train_xtractor.py --dataset $dataset_file --model $model_file --training $training_file
